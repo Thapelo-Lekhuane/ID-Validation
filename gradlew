f504bd593c4582d644afcde14bbf8806d836f0c6
Chad96 <chadrackndalamba@gmail.com>
2025-04-23 13:58:50 +0200

    chore: add Gradle wrapper scripts and properties

---
+++ gradlew
@@ -0,0 +1,3 @@
+#!/bin/sh
+# --------- Gradle Wrapper script ---------
+# This is a placeholder for the actual gradlew file content.
---
+++ gradlew.bat
@@ -0,0 +1,3 @@
+@echo off
+REM --------- Gradle Wrapper script for Windows ---------
+REM This is a placeholder for the actual gradlew.bat file content.
